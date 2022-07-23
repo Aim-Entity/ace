dc3fe4cf33135499da08054a0e7342e74e396e45
Aim-Entity <59455267+Aim-Entity@users.noreply.github.com>
2022-07-23 18:42:34 +0100

    contact responsive

--- home/models.py
+++ home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Testimonial(models.Model):
+  name = models.CharField(max_length=50, null=True)
+  title = models.CharField(max_length=100, null=True, blank=True)
+  quote = models.CharField(max_length=1000, null=True)
+  #image = models.ImageField()
+  
+  def __str__(self):
+    return self.name
+  
+class Job(models.Model):
+  name = models.CharField(max_length=200, null=True)
+  image = models.ImageField(upload_to="companies", null=True)
+  
+  def __str__(self):
+    return self.name
+  
